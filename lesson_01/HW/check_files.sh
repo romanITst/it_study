891f789d21db879981a75d53b3bd0f9c8ff42476
romanITst <ivanov2011roma@yandex.by>
2023-10-17 18:30:47 +0300

    some fixes 2

--- lesson_01/HW/check_files.sh
+++ lesson_01/HW/check_files.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 
-[ -f "data.txt" ] && echo "data.txt was found" || echo "data.txt is missing. The 'data.txt' file will be created in this directory with no reason. Check it - put the 'ls' command to your command line. Now you can execute script again and see what will be happen" && touch data.txt && echo "This file was created automatically by script 'check_files.sh' for training my bash knowledge" > data.txt
+[ -f "data.txt" ] && echo "data.txt was found" || echo "data.txt is missing"
 
-exit 0
