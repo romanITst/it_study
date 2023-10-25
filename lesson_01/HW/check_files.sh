aac270d58acb038632dfd1965e40043bc9672abf
romanITst <ivanov2011roma@yandex.by>
2023-10-25 16:20:24 +0300

    Unecessary files and dirs was removed

--- lesson_01/HW/check_files.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-[ -f "data.txt" ] && echo "data.txt was found" || echo "data.txt is missing"
-
--- lesson_01/HW/fd2.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-echo "This is the standard output that will be silenced" > /dev/null
-echo "This is the error msg that wil be silenced" > /dev/null >&2
-exit 0
