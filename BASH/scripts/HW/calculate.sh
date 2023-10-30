1d1c833e7598c7b59c4690cc5c05c617d33fe899
romanITst <ivanov2011roma@yandex.by>
2023-10-30 19:27:11 +0300

    TAB space before echo's was removed

--- BASH/scripts/HW/calculate.sh
+++ BASH/scripts/HW/calculate.sh
@@ -9,7 +9,7 @@ if [ -z ${1} ] || [ -z ${2} ]; then
 	echo "You should to put 2 integer values. Ex.: 2 2. Exit"; exit
 fi
 
-	echo "sum: $(($1+$2))"
-	echo "product: $(($1*$2))"
-	echo "quotience: $(($1/$2))   #This is using int division"
-	echo "difference: $(($1-$2))"
+echo "sum: $(($1+$2))"
+echo "product: $(($1*$2))"
+echo "quotience: $(($1/$2))   #This is using int division"
+echo "difference: $(($1-$2))"
