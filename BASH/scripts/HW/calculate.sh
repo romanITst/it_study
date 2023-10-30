191f5cbc481634b14e613c855192abd3c5c8c6f9
romanITst <ivanov2011roma@yandex.by>
2023-10-30 19:24:36 +0300

    Block structure of code was removed. Calculate actions was moved from own block into echo's output. Echo's were released separately from 'if'. Added a little more of #Description.

--- BASH/scripts/HW/calculate.sh
+++ BASH/scripts/HW/calculate.sh
@@ -1,28 +1,15 @@
 #!/usr/bin/bash
 
 
-# Description: Calculator
+# Description: Basic calculator. We should to put 2 any integers
+# Example: 2 2 / -87 93 / -74 -17
 
 
 if [ -z ${1} ] || [ -z ${2} ]; then
-	echo "You should to put 2 integer values using space key. Ex.: 2 2"
-else
-
-#calculate_block
-#
-x=${1}
-y=${2}
-sum=$((x+y))
-product=$((x*y))
-quotience=$((x/y))
-difference=$((x-y))
-
-
-#echo_block
-#
-echo "sum: ${sum}"
-echo "product: ${product}"
-echo "quotience: ${quotience}   #Note: This is using integer division"
-echo "difference: ${difference}"
-
+	echo "You should to put 2 integer values. Ex.: 2 2. Exit"; exit
 fi
+
+	echo "sum: $(($1+$2))"
+	echo "product: $(($1*$2))"
+	echo "quotience: $(($1/$2))   #This is using int division"
+	echo "difference: $(($1-$2))"
