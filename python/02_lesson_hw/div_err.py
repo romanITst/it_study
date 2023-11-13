38e58ae6d85c07418815c58cb6bf1f8416ca58a4
romanITst <ivanov2011roma@yandex.by>
2023-11-13 22:05:40 +0300

    String with bad code deleted. You should not to see this. ;(

--- python/02_lesson_hw/div_err.py
+++ python/02_lesson_hw/div_err.py
@@ -12,13 +12,11 @@ except ValueError:
     print ("Invalid input. You should to use only numbers.")
     print ("Try again")
     exit()
-else:
-    result = num1 / num2
 
 
 try:    # result 'try'
     result = num1 / num2
 except ZeroDivisionError:
-    print ("Division by zero is not allowed. Do NOT use '0'")
+    print ("Division by zero is not allowed. Do NOT use '0' Try Again")
 else:
     print ("Your answer for division: ", result)
