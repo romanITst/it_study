4d4ba87c6fac30aee4d69279372f3c64fc0aa2ee
romanITst <ivanov2011roma@yandex.by>
2023-11-21 15:14:22 +0300

    new script was added

---
+++ python/03_lesson_functions/02_divide.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+# Description: With this script you can divide the dividend by divisor.
+
+def divide(dividend, divisor): #PARAMETERS are: dividend & divisor
+
+    '''Divide the dividend by divisor
+    param dividend: Number what will divide by divisor
+    param divisor: Number by which dividend will divide
+    return: The result of the division'''
+
+    return dividend / divisor
+
+result = divide(15, 5) #ARGUMENTS are: 15 & 5
