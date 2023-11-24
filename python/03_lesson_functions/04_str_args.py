0ec25eda689b0c9b8869eed86211f4aa824edd3e
romanITst <ivanov2011roma@yandex.by>
2023-11-24 17:14:05 +0300

    Description of function was added

--- python/03_lesson_functions/04_str_args.py
+++ python/03_lesson_functions/04_str_args.py
@@ -3,6 +3,11 @@
 # Description: This script shows how to use *args with strings.
 
 def strings(*args):
+    '''
+    Concatenate list of strings.
+    param args: Strings to concatenate.
+    return: The concatenated string.
+    '''
     result = ""
     for s in args:
         result += s
