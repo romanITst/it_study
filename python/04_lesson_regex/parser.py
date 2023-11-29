642631b8133230ac6b7f0ef135ceb2c4604dbe71
romanITst <ivanov2011roma@yandex.by>
2023-11-29 14:10:50 +0300

    line_report function added

--- python/04_lesson_regex/parser.py
+++ python/04_lesson_regex/parser.py
@@ -5,14 +5,21 @@
 
 import sys
 
-
+line = r'127.0.0.1 - - [28/Nov/2023:17:08:07 +0300] "GET / HTTP/1.1" 200 396 "-" "python-requests/2.25.1"'
 def splitting_line(line):
+    """Return splitted line"""
     splitted_line = line.split()
     return splitted_line
 
+splitted_line = splitting_line(line)
 
-# line = r'127.0.0.1 - - [28/Nov/2023:17:08:07 +0300] "GET / HTTP/1.1" 200 396 "-" "python-requests/2.25.1"'
-# splitted_line = line.split()
-# ipaddr = line[3]
-# print(splitted_line)
-# print(ipaddr)
+
+def line_report(splitted_line): 
+    ipaddres = splitted_line[0]
+    bytes_sent = splitted_line[9]
+    logline = {f"IPaddres": ipaddres, "Bytes sent": bytes_sent}
+    return logline
+
+logline = line_report(splitted_line)
+print(logline)
+
