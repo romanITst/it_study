52cc5c4bd7849f342647a4e292d9f7e7eac06bfe
romanITst <ivanov2011roma@yandex.by>
2023-12-06 21:06:33 +0300

    Process of opening and writing to file was added to separate function. User's choice was added to existance check

--- python/05_os.module/test_file_generator.py
+++ python/05_os.module/test_file_generator.py
@@ -1,21 +1,33 @@
 #!/usr/bin/env python3
 
 # Description: This script is opening file (or creating if it doesn't exist)
-# and writing to it information you need.
+# and writing to it information you need. 
 
 
 import sys
 import os
 
+
+def main_function(file_name):
+    with open(file_name, "a+") as file:
+        for i in range(int(sys.argv[3])):
+            file.write(str(sys.argv[2]))
+            file.write("\n")
+    file.close()
+    print("Text was added to file!")
+
+
 if os.path.exists(str(sys.argv[1])) == True:
     print("File is exist! Writing text to a file...")
+    main_function(str(sys.argv[1]))
+    
 if os.path.exists(str(sys.argv[1])) == False:
-    print("The file is doesn't exist. Creating file...")
-    print("Writing text to a file...")
+    print("The file is doesn't exist. Want to create it? Type 'y' or 'n'")
+    if str(input()) == "y":
+        main_function(str(sys.argv[1]))
+        sys.exit()
+    else:
+        print("Exit...")
+        sys.exit()
 
-with open(str(sys.argv[1]), "a+") as file:
-    for i in range(int(sys.argv[3])):
-        file.write(str(sys.argv[2]))
-        file.write("\n")
-file.close()
-print("Text was added to file!")
+
