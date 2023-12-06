d9b858c4ed621513f423dd79aa81f4c4d4872571
romanITst <ivanov2011roma@yandex.by>
2023-12-06 19:44:58 +0300

    writing mode was added to opening function. sys.module was imported. Transfer of information to the script is implemented via sys.module

--- python/05_os.module/00_write_fo_file.py
+++ python/05_os.module/00_write_fo_file.py
@@ -1,12 +1,21 @@
 #!/usr/bin/env python3
 
 # Description: This script is opening file (or creating if it doesn't exist)
-# and writing to it needed information.
+# and writing to it information you need.
 
 
-def opening_file():
-    file_name = str(input("Enter the name of the file: "))
-    with open(file_name, "w") as file_name:
+import sys
+
+
+file_name = sys.argv[1]
+
+def opening_and_writing_to_file(file_name):
+    with open(file_name, "a+") as file:
         print("File is ready to writing")
+        for i in range(int(sys.argv[3])):
+            file.write(str(sys.argv[2]))
+            file.write("\n")
+    file.close()
+    print("Information was added to file")
 
-opening_file()
+opening_and_writing_to_file(file_name)
