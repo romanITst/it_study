5b0618066462b8ad96030283c19df4fe86104295
romanITst <ivanov2011roma@yandex.by>
2023-12-06 19:03:01 +0300

    'opening file' function was added

--- python/05_os.module/00_write_fo_file.py
+++ python/05_os.module/00_write_fo_file.py
@@ -1,4 +1,12 @@
 #!/usr/bin/env python3
 
-# Description: This script is writing to file needed count of string
+# Description: This script is opening file (or creating if it doesn't exist)
+# and writing to it needed information.
 
+
+def opening_file():
+    file_name = str(input("Enter the name of the file: "))
+    with open(file_name, "w") as file_name:
+        print("File is ready to writing")
+
+opening_file()
