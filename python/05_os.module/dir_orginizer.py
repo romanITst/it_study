94b35962d4e0c2c02aa86846f852b10cb418f85c
romanITst <ivanov2011roma@yandex.by>
2023-12-07 19:02:45 +0300

    Discriptions for functions was added. Functions 'creating_folder' & 'creating_folders_by_extensions' added

--- python/05_os.module/dir_orginizer.py
+++ python/05_os.module/dir_orginizer.py
@@ -9,6 +9,10 @@ import os
 import sys
 
 def get_list_of_files(main_path):
+    '''
+    This function returns file names
+    in dir you entered
+    '''
     for foldername, subfolders, filenames in os.walk(main_path):
         return filenames
     return filenames
@@ -16,12 +20,19 @@ def get_list_of_files(main_path):
 list_of_files = get_list_of_files(str(sys.argv[1]))
 
 def get_file_extension(file_name):
+    '''
+    This script returns file extension
+    '''
     file_name_wout_point = file_name.replace(".", " ")
     splitted_file_name = file_name_wout_point.split()
     file_extension = splitted_file_name[-1]
     return file_extension
 
 def get_list_of_unique_extensions(list_of_files):
+    '''
+    This script returns list of
+    unique extensions in dir
+    '''
     list_of_extensions = []
     for files in list_of_files:
         list_of_extensions.append(get_file_extension(files))
@@ -31,5 +42,20 @@ def get_list_of_unique_extensions(list_of_files):
         list_of_unique_extensions.append(exts)
     return list_of_unique_extensions
 
+def creating_folder(folder_name):
+    os.mkdir(folder_name)
+    return folder_name
+
+def creating_folders_by_extensions(unique_extensions):
+    list_of_files = get_list_of_files(main_path=str(sys.argv[1]))
+    unique_extensions = get_list_of_unique_extensions(list_of_files)
+    list_of_created_folders = []
+    for exts in unique_extensions:
+        created_folder = creating_folder("folder.{}".format(exts))
+        list_of_created_folders.append(created_folder)
+    return list_of_created_folders
+
 unique_extensions = get_list_of_unique_extensions(list_of_files)
-print(unique_extensions)
+
+print(creating_folders_by_extensions(unique_extensions))
+    
