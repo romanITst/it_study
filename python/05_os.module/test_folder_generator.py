8c9f083c51e8100cd02fa029f54872eb0fd179b7
romanITst <ivanov2011roma@yandex.by>
2023-12-07 16:23:54 +0300

    #Description changes

--- python/05_os.module/test_folder_generator.py
+++ python/05_os.module/test_folder_generator.py
@@ -1,18 +1,19 @@
 #!/usr/bin/env python3
 
 # Description: This script creates a target folder(if it does not exist),
-# and 10 folders in it, and 10 folders in each of this. Example: 
+# and 10 folders in it, and 10 folders in each of this and 10 files in last folders. 
 # input:                                                        
-# folder_generator.py [1] [2] [3] [4] [5]
+# folder_generator.py [1] [2] [3] [4]
 # [1] - main_folder / [2] - subfolder_1_lvl / [3] - subfolder_2_lvl                   
 # [4] - files
 # folder_generator.py music artist album song
 # output: music
 #         ├── artist00
 #         │   ├── album00
-#         │   ├── album01
-#         │   ...
-#         │   └── album07
+#         │       ├── song00
+#         │       ...   
+#         │       └──song09
+#         │   └── album09
 #         ├── artist01
 #         ...
 
@@ -23,16 +24,24 @@ import sys
 
 def creating_folder(folder_name):
     '''
-    This function is creates folder
+    This function creates folder
     with necessary name or path
     and returns its name/path
     '''
     created_folder = os.mkdir(folder_name)
     return created_folder
 
-def creating_file(file):
-    with open(file, "w+") as file:
-        file.close()
+
+def creating_file(file_name):
+    '''
+    This function creates folder
+    with necessary name or path
+    and returns its name/path
+    '''
+    with open(file_name, "w+") as created_file:
+        created_file.close()
+    return created_file
+
 
 def creating_subfolders():
     for i in range(10):
@@ -44,8 +53,8 @@ def creating_subfolders():
             creating_folder(lvl_2_folder)
             os.chdir(lvl_2_folder)
             for g in range(10):
-                file = str(sys.argv[4] + "0{}".format(g))
-                creating_file(file)
+                file_name = str(sys.argv[4] + "0{}".format(g))
+                creating_file(file_name)
             os.chdir("..")
         os.chdir("..")
 
@@ -56,7 +65,7 @@ if os.path.exists(str(sys.argv[1])):
     print("Directory you entered is already exists. Continue?")
     if str(input("[yes/no]: ")) == "yes":
         os.chdir(str(sys.argv[1]))
-        print("Creating subfolders...")
+        print("Creating subfolders and files...")
         creating_subfolders()
         print("Done.")
         sys.exit(1)
@@ -70,7 +79,7 @@ if not os.path.exists(str(sys.argv[1])):
         print("Creating target folder...")
         creating_folder(str(sys.argv[1]))
         os.chdir(str(sys.argv[1]))
-        print("Creating subfolders...")
+        print("Creating subfolders and files...")
         creating_subfolders()
         print("Done.")
         sys.exit(1)
