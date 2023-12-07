d7a045e0e2320b196b89d57a7f165246a4386bfa
romanITst <ivanov2011roma@yandex.by>
2023-12-07 14:42:16 +0300

    for loop included in 'creating_subfolders function'. Existence check with user's input support was added.

--- python/05_os.module/folder_generator.py
+++ python/05_os.module/folder_generator.py
@@ -18,14 +18,42 @@ def creating_folder(folder_name):
     created_folder = os.mkdir(folder_name)
     return created_folder
 
-creating_folder(str(sys.argv[1]))
-os.chdir(str(sys.argv[1]))
 
-for i in range(int(sys.argv[3])):
-    lvl_1_folder = str(sys.argv[2] + "0{}".format(i))
-    creating_folder(lvl_1_folder)
-    os.chdir(lvl_1_folder)
-    for s in range(int(sys.argv[5])):
-        lvl_2_folder = str(sys.argv[4] + "0{}".format(s))
-        creating_folder(lvl_2_folder)
-    os.chdir("..")
+def creating_subfolders():
+    for i in range(int(sys.argv[3])):
+        lvl_1_folder = str(sys.argv[2] + "0{}".format(i))
+        creating_folder(lvl_1_folder)
+        os.chdir(lvl_1_folder)
+        for s in range(int(sys.argv[5])):
+            lvl_2_folder = str(sys.argv[4] + "0{}".format(s))
+            creating_folder(lvl_2_folder)
+        os.chdir("..")
+
+if os.path.exists(str(sys.argv[1])):
+    try:
+        creating_folder(str(sys.argv[1]))
+    except: FileExistsError
+    print("Directory you entered is already exists. Continue?")
+    if str(input("[yes/no]: ")) == "yes":
+        os.chdir(str(sys.argv[1]))
+        print("Creating subfolders...")
+        creating_subfolders()
+        print("Done.")
+        sys.exit(1)
+    else:
+        print("You chose 'no'. Exit.")
+        sys.exit(1)
+
+if not os.path.exists(str(sys.argv[1])):
+    print("Directory you entered doesn't exists. Create it?")
+    if str(input("[yes/no]: ")) == "yes":
+        print("Creating target folder...")
+        creating_folder(str(sys.argv[1]))
+        os.chdir(str(sys.argv[1]))
+        print("Creating subfolders...")
+        creating_subfolders()
+        print("Done.")
+        sys.exit(1)
+    else:
+        print("You chose 'no'. Exit.")
+        sys.exit(1)
