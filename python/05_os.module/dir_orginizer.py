1f32809230695b01be50f1406b52cfe94945f02a
romanITst <ivanov2011roma@yandex.by>
2023-12-07 18:35:23 +0300

    Function 'get_list_of_unique_extensions' added

--- python/05_os.module/dir_orginizer.py
+++ python/05_os.module/dir_orginizer.py
@@ -8,10 +8,12 @@
 import os
 import sys
 
-def get_list_of_file(main_path):
+def get_list_of_files(main_path):
     for foldername, subfolders, filenames in os.walk(main_path):
-        print("Filenames: ", filenames)
+        return filenames
     return filenames
+
+list_of_files = get_list_of_files(str(sys.argv[1]))
 
 def get_file_extension(file_name):
     file_name_wout_point = file_name.replace(".", " ")
@@ -19,10 +21,15 @@ def get_file_extension(file_name):
     file_extension = splitted_file_name[-1]
     return file_extension
 
+def get_list_of_unique_extensions(list_of_files):
+    list_of_extensions = []
+    for files in list_of_files:
+        list_of_extensions.append(get_file_extension(files))
+    list_of_unique_extensions = []
+    unique_extensions = set(list_of_extensions)
+    for exts in unique_extensions:
+        list_of_unique_extensions.append(exts)
+    return list_of_unique_extensions
 
-file_extension = get_file_extension("file02.txt")
-print(file_extension)
-    
-
-
-#get_list_of_file(str(sys.argv[1]))
+unique_extensions = get_list_of_unique_extensions(list_of_files)
+print(unique_extensions)
