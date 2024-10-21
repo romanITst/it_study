3621b103061339e9fbef58c32c570c6f5342bf1c
romanITst <rkurtasov@gmail.com>
2024-10-21 20:19:42 +0000

    description added

--- BASH/scripts/user add.sh
+++ BASH/scripts/user add.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 
-# Description: 
-#
-# 
-
+# Description: This script creating the user with username
+# you want. If user with the same name is exists the script
+# will report it.
 
 
 read -p "Enter the user name you want to add: " username
 
 if grep -w $username /etc/passwd
 then
-	echo "User with same username is exsisting. Terminate..."
+	echo "User with same username is existing. Terminate..."
 else	
 	echo "Adding new user..."
 	sudo useradd $username
