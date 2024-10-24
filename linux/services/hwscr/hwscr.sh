acfd5751a98864c15199ab3c015b7c3eac8aa64c
romanITst <rkurtasov@gmail.com>
2024-10-24 16:23:15 +0000

    Init. 3 homework files: the service, the script and the result files.

---
+++ linux/services/hwscr/hwscr.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+# Description: Script which echo the information to file.
+# Starting by /etc/systemd/system/hwscr.service
+
+for i in {1..10}; do	
+	echo "$i hwscr.service" >> ~/it-training/linux/services/hwscr-result.txt
+done
+
